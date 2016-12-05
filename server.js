2d852c43c477c8937a950a096839f726cba52ef0
Yasser Hussain <yasserhussain1110@gmail.com>
2016-12-05 16:12:29 +0530

    changed ip finding mechanism

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ var useragent = require('useragent');
 
 var server = http.createServer(function(req, res){
   if (req.url === '/api/whoami' && req.method === 'GET') {
-    var ip = req.connection.remoteAddress.replace(/^::ffff:/, '');
+
+    var ip = req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
+    ip = ip.replace(/^::ffff:/, '');
+
     var agent = useragent.parse(req.headers['user-agent']);
     var language = req.headers["accept-language"].replace(/[;,].*/, '')
     var headers = req.headers;
